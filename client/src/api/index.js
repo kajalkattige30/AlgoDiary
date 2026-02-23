c6217f85a662b69c6eb89f7201b39b2b43893bed
kajal <kajalk3098@gmail.com>
2026-02-23 22:23:38 +0530

    fix: configure server port and connect client to backend API

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; // To make api calls
 
-const url = "http://localhost:5000/posts";
+const url = "http://localhost:5001/posts";
 
 export const fetchPosts = () => axios.get(url); // localhost:5000/posts returns all the posts that we have in the database
 
